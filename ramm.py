77eed96a6fd1fee9cb06020ccf402156347060d0
nikesh45 <nikeshkhadka45@gmail.com>
2018-02-09 14:44:57 +0545

    sum

---
+++ ramm.py
@@ -0,0 +1,10 @@
+x=(input("enter the character a for add and s for sub"))
+c=int(input("enter the first one")) 
+d=int(input("enter the second one"))
+if x == 'a':
+	print(c+d)
+elif x == 'b':
+	print(c-d)
+else:
+	print("invalid") 
+	
